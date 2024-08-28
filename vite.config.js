bcc098ca476a1fe664c1fb21e951e24f7febbe71
Tom Hinkle <tmhinkle@gmail.com>
2024-08-28 23:25:34 +0000

    Fix packaging of types

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import dts from "vite-plugin-dts";
+import fs from "fs";
+import path from "path";
 
 export default defineConfig({
   build: {
@@ -21,6 +23,16 @@ export default defineConfig({
   },
   plugins: [
     cssInjectedByJsPlugin(), // Inline CSS into JS
-    dts(),
+    dts({
+      rollupTypes: true,
+      afterBuild: () => {
+        // Rename the generated type file
+        const oldPath = path.resolve(__dirname, 'dist', 'index.d.ts');
+        const newPath = path.resolve(__dirname, 'dist', 'text-interface.d.ts');
+        if (fs.existsSync(oldPath)) {
+          fs.renameSync(oldPath, newPath);
+        }
+      }
+    }),
   ],
 });
