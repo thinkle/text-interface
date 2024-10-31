083f3c56d35ae8dfb01956528ffb367a58430039
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-10-31 19:46:11 +0000

    Fix range methods. Add prompt methods as a convenience

--- test/main.js
+++ test/main.js
@@ -3,10 +3,12 @@ console.log("Off to a great start!");
 // Example usage of your library for testing
 let app = document.querySelector("#app");
 let ti = new TextInterface(app, "Testing Text Interface");
+
 ti.shouldStealFocus = true;
-await ti.output("What is your name?");
-let name = await ti.readText();
+let name = await ti.prompt("What is your name?");
+let num = await ti.promptIntegerInRange("Enter a number between 1 and 10!", 1, 10);
 ti.output("Hello, " + name);
+ti.output(`${num+1} is my favorite number - so close!`);
 let choice = await ti.readChoice([
   "Eat an apple",
   "Eat a cookie",
