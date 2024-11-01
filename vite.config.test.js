9b0a9f66c57d49d1ed3dd217970f68c20112bc48
Tom Hinkle <tmhinkle@gmail.com>
2024-11-01 07:51:01 -0400

    For easy testing on local network

--- vite.config.test.js
+++ vite.config.test.js
@@ -10,5 +10,7 @@ export default defineConfig({
   },
   server: {
     open: "/index.html", // Automatically open the browser to this file
+    host: "0.0.0.0", // This binds the server to all available network interfaces
+    port: 80, // Optional: specify a port (default is 5173)
   },
 });
